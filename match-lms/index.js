af7b2363f9f2fd1db32c799dcfe403a42ed421fe
Scy Akulick <Scy.Akulick@D2L.com>
2021-11-19 11:45:54 -0500

    async

--- match-lms/index.js
+++ match-lms/index.js
@@ -2,13 +2,15 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const lmsVersionHelper = require('./lms-version-helper');
 
-try {
-    const version = await lmsVersionHelper.tryGetActiveDevelopmentRelease()
-    console.log(version + ' tryGetActiveDevelopmentRelease');
-    core.setOutput("lmsVersion", version);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    //console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
+async function run() {
+    try {
+        const version = await lmsVersionHelper.tryGetActiveDevelopmentRelease()
+        console.log(version + ' tryGetActiveDevelopmentRelease');
+        core.setOutput("lmsVersion", version);
+        // Get the JSON webhook payload for the event that triggered the workflow
+        const payload = JSON.stringify(github.context.payload, undefined, 2);
+        //console.log(`The event payload: ${payload}`);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
