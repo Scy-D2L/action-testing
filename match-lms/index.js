cf1074f637baef0324933b512000c6417e6ca294
Scy Akulick <Scy.Akulick@D2L.com>
2021-11-19 11:39:03 -0500

    getlmsver id

--- match-lms/index.js
+++ match-lms/index.js
@@ -3,9 +3,6 @@ const github = require('@actions/github');
 const lmsVersionHelper = require('./lms-version-helper');
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
     core.setOutput("lmsVersion", lmsVersionHelper.tryGetActiveDevelopmentRelease())
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
